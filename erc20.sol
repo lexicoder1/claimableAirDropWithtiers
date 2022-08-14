b6f81c9bb51192d5e66c5dad5924e4cab33c93bc
lexicoder1 <uzorifeanyi1000@gmail.com>
2022-08-13 23:54:21 -0700

    update

--- erc20.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity 0.8.0;
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
-
-contract erc20 is ERC20 {
-    
-    constructor() ERC20("",""){
-      _mint(msg.sender,100000*10**18);
-    }
-}
